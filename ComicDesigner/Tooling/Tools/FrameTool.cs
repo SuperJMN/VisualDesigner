8248e7fffe4db0f65f58ad72499072b9febd6dc7
SuperJMN <superjmn@outlook.com>
2014-02-21 00:11:50 +0100

    Untracked FrameTool forgotten! Added

---
+++ ComicDesigner/Tooling/Tools/FrameTool.cs
@@ -0,0 +1,26 @@
+using Glass.Design.WinRT.Annotations;
+using Model;
+using StyleMVVM.DependencyInjection;
+
+namespace ComicDesigner.Tooling.Tools
+{
+    [Export(typeof(ITool)), UsedImplicitly]
+    public class FrameTool : Tool
+    {
+        public FrameTool()
+        {
+            Name = "Frame";
+            IconKey = "Frame";
+        }
+        
+        public override CanvasItemViewModel CreateItem()
+        {
+            return new Frame
+                   {
+                       Width = 500, 
+                       Height = 400,
+                   };
+        }
+        
+    }
+}
