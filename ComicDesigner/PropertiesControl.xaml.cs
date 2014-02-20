501c989dcbbbcddf8de1cd5ab3d31cf8798decdc
SuperJMN <superjmn@outlook.com>
2014-02-20 18:51:16 +0100

    Added bottom bar.

---
+++ ComicDesigner/PropertiesControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Glass.Design.Pcl.Canvas;
+
+namespace ComicDesigner
+{
+    public sealed partial class PropertiesControl
+    {
+        public PropertiesControl()
+        {
+            this.InitializeComponent();
+        }
+
+        #region SelectedItem
+        public static readonly DependencyProperty SelectedItemProperty =
+          DependencyProperty.Register("SelectedItem", typeof(CanvasItem), typeof(PropertiesControl),
+            new PropertyMetadata(null));
+
+        public bool SelectedItem
+        {
+            get { return (bool)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        #endregion        
+    }
+}
